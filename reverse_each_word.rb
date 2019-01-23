8d621f7d876445747cac721bb83ee94b92668a60
Marci Clark <marcig612@gmail.com>
2019-01-23 19:17:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,7 +16,8 @@ def reverse_each_word("Hello there, and how are you?")
     hello_array << string.reverse 
   end
   
-  reverse_each_word
+  reverse_each_word("Hello there, and how are you?")
+end 
     
 
   
