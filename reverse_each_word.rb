240c9658a85d9cfc78d1377247805947f20e57be
Marci Clark <marcig612@gmail.com>
2019-01-23 19:14:49 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each("Hello there, and how are you?")
+  
+array = string.split(" ")
+return array = []
+array.each do |string|
+return_array << string.reverse
+end 
+
+return_array.join (" ")
+end 
+
+def reverse_each_word("Hello there, and how are you?")
+  array = string.split(" ")
+  
+
+  
